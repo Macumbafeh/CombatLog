1f7e5c96ab88df3c197ca0676e1a00f95227362c
Schaka <schakaqt@hotmail.com>
2013-11-05 11:33:59 +0000

    add some different event handlings

--- CombatLog.lua
+++ CombatLog.lua
@@ -1,6 +1,5 @@
 local frame = CreateFrame("Frame", nil, UIParent)
 local function combatlog(self, event, ...)
-		
         local timestamp, event, sourceGUID,sourceName,sourceFlags,destGUID,destName,destFlags,spellID,spellName = select(1, ...)
 		if not timestamp then timestamp = "noTimestamp" end
 		if not event then event = "noEvent" end
@@ -12,7 +11,17 @@ local function combatlog(self, event, ...)
 		if not destFlags then destFlags = "noDestFlags" end
 		if not spellID then spellID = "noSpelLID" end
 		if not spellName then spellName = "noSpellName" end
-		DEFAULT_CHAT_FRAME:AddMessage(timestamp.."  "..event.."  "..sourceGUID.."  "..sourceName.."  "..sourceFlags.."  "..destGUID.."  "..destName.."  "..destFlags.."  "..spellID.."  "..spellName)
+		--DEFAULT_CHAT_FRAME:AddMessage(timestamp.."  "..event.."  "..sourceGUID.."  "..sourceName.."  "..sourceFlags.."  "..destGUID.."  "..destName.."  "..destFlags.."  "..spellID.."  "..spellName)]]
+		
+		if event == "SPELL_DISPEL" then
+			timestamp, event, sourceGUID,sourceName,sourceFlags,destGUID,destName,destFlags,spellID, spellName, arg14, arg15, arg16, arg17, arg18 = select(1, ...)
+			DEFAULT_CHAT_FRAME:AddMessage(event.."  "..arg14.."  "..arg15.."  "..arg16.."  "..arg17.."  "..arg18.."  "..arg19)
+		end
+		
+		if event == "SPELL_CAST_FAILED" then
+			timestamp, event, sourceGUID,sourceName,sourceFlags,destGUID,destName,destFlags,spellID, spellName, arg14, arg15 = select(1, ...)
+			DEFAULT_CHAT_FRAME:AddMessage(event.."  "..arg14.."  "..arg15)
+		end
 end
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 frame:SetScript("OnEvent", combatlog)
